39b24fb11fc4de2de2a46a9f89d52dcf55827f53
Victor Sevilla <>
2020-09-10 17:33:39 -0500

    primer commit consola especialistas

---
+++ android/item-list/style.js
@@ -0,0 +1,63 @@
+
+import { StyleSheet } from "react-native";
+
+const Stylestem = StyleSheet.create({    
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    itemStatus: {
+      fontFamily: 'sans-serif',
+      fontSize: 13,
+      backgroundColor: '#E2F0FF',
+      color: 'blue',
+      width: 80,
+      marginLeft: 59,
+      paddingLeft: 18,
+    },
+    itemLine: {
+      paddingLeft: 5,
+      paddingTop: 10,
+    },
+    itemProject: {
+      paddingLeft: 59,
+      fontSize: 15,
+      color: '#000000',
+      fontFamily: 'sans-serif',
+    },
+    itemId: {
+      fontFamily: 'sans-serif',
+      paddingLeft: 59,
+      paddingTop: 16,
+      paddingBottom: 9,
+      fontSize: 13,
+      color: '#999999',
+    },
+    itemServices: {
+      fontFamily: 'sans-serif',
+      paddingLeft: 59,
+      fontSize: 13,
+      color: '#000000',
+    },
+    itemSubject: {
+      fontFamily: 'sans-serif',
+      paddingLeft: 59,
+      fontSize: 13,
+      color: '#999999',
+    },
+    title: {
+      fontSize: 32,
+    },
+    logo: {
+      width: 30,
+      height: 30,
+      tintColor: '#35527a',
+      position: 'absolute',
+      marginTop: 37,
+      marginLeft: 24,
+    },
+  });
+
+  export default Stylestem;

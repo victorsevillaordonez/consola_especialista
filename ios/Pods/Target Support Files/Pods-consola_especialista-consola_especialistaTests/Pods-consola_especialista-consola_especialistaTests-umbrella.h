26854e90b9f291d9cea9ff63e993224589b4e624
Victor Sevilla <>
2020-09-14 10:48:05 -0500

    Creada nueva estructura

---
+++ ios/Pods/Target Support Files/Pods-consola_especialista-consola_especialistaTests/Pods-consola_especialista-consola_especialistaTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_consola_especialista_consola_especialistaTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_consola_especialista_consola_especialistaTestsVersionString[];
+
